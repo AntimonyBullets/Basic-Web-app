9daba737d87d164c3a9638774fd13916735487b4
AntimonyBullets <antimonybullets@gmail.com>
2024-12-17 21:05:29 +0530

    commit-1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req,res)=>{
 
 //If GET request is sent on the route passed as first parameter here ('/login' in the following case), which means if someone tries to access this url, we are sending a response will be sent which will be displayed on the web browser. We use 'res.send()' to send response.
 app.get('/login', (req, res)=>{
-    res.send("<h1>Log in through this page</h1>");
+    res.send("<h1>You can Log in through this page</h1>");
 });
 app.listen(process.env.PORT, ()=>{
     console.log(`App listening on port ${process.env.PORT}`)
